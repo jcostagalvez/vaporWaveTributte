115e5d5062845f091a5a78a5daaa5d1844c9a49b
Jesús Costa <jcostagalvez@gmail.com>
2023-01-26 19:37:51 +0100

    diabolo on scene

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import './stilos.css'
-import gsap from 'gsap';
-import { pickerColor } from './colorPicker';
-import gsapCore from 'gsap/gsap-core';
-import { ObjectLoader } from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 const size = {
   height: window.innerHeight,
   width: window.innerWidth
 };
 
+const canvas = document.querySelector('.miguelangel')
+
 //The scene is our stage
 const scene = new THREE.Scene();
+
+// Vamos a cargar la imagen de forma asyncronica y añadirla a nuestro escenario
 const loader = new GLTFLoader();
 loader.load( './public/Sources/Imagen/scene.gltf', function ( gltf ) {
 	scene.add( gltf.scene );
-  const canvas = document.querySelector(".geometryFigure");
-  const renderer = new THREE.WebGLRenderer({ canvas });
-  renderer.setSize(size.width, size.height);
-  renderer.render(scene, camera);
-  const camera = new THREE.PerspectiveCamera(45, size.width/size.height);
-  camera.position.z = 14;
-  scene.add(camera);
-  
 }, undefined, function ( error ) {
 
 	console.error( 'error' );
 
 } );
+
+// Ahora creamos las luces
+const light = new THREE.DirectionalLight('#F538F2', 2);
+light.position.set(2, 2, 5);
+scene.add(light);
+
+const light2 = new THREE.DirectionalLight('#00ff00', 4);
+light.position.set(3, 3, 8);
+scene.add(light2);
+// creamos la camara y la perspectiva
+
+const camera = new THREE.PerspectiveCamera(75, size.width/size.height, 0.1, 100);
+camera.position.set(0, 1, 2);
+scene.add(camera);
+
+const rendered = new THREE.WebGL1Renderer({ canvas: canvas});
+
+rendered.setSize(size.width, size.height);
+rendered.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+rendered.shadowMap = true;
+rendered.gammaOutput = true;
+
+const loop = () => {
+  rendered.render (scene, camera);
+  window.requestAnimationFrame(loop);
+}
+
+loop();
